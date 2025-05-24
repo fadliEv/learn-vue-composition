0e5554d8def8c4743702f620c3ece1675cd1ec3e
fadli <fadlirahmanfauzan04@gmail.com>
2025-05-24 12:32:45 +0700

    interval timer with lifecycle

--- src/lifecycle/Index.vue
+++ src/lifecycle/Index.vue
@@ -1,31 +1,15 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue'
-
-const timer = ref(0)
-let intervalId = null
-
-// Debug: Cek saat komponen pertama kali dimuat
-onMounted(() => {
-  console.log('Component Mounted')
-  intervalId = setInterval(() => {
-    timer.value++
-  }, 1000)
-})
-
-// Debug: Cek saat komponen di-unmount
-onUnmounted(() => {
-  clearInterval(intervalId)
-  console.log('Component Unmounted')
-})
+import {ref} from 'vue'
+import Timer from './Timer.vue';
 
 const isVisible = ref(true)
 </script>
 
 <template>
   <button @click="isVisible = !isVisible">
-    {{ isVisible ? 'Hide' : 'Show' }} Timer
+    {{ isVisible ? "Hide" : "Show" }}
   </button>
 
-  <!-- Gunakan v-if agar komponen bisa di-unmount -->
-  <p v-if="isVisible">Timer {{ timer }}</p>
+  <Timer v-if="isVisible"/>
+
 </template>
